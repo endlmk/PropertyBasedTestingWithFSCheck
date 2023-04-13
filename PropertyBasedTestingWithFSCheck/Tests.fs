5be910d24d47c67fdd6c20a5f101b1b253d15398
endlmk <1269467+endlmk@users.noreply.github.com>
2023-04-13 23:31:48 +0900

    Add property based  add test.

--- PropertyBasedTestingWithFSCheck/Tests.fs
+++ PropertyBasedTestingWithFSCheck/Tests.fs
@@ -2,10 +2,15 @@ module Tests
 
 open System
 open Xunit
+open FsCheck
+open FsCheck.Xunit
 
-let add x y = 0
-
+let add x y = x + y
 
 [<Fact>]
 let ``Add test`` () =
     Assert.Equal(3, add 1 2)
+
+[<Property>]
+let ``add = x + y`` x y = 
+    add x y = x + y
