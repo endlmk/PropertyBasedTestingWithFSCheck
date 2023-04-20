1106566685866431761d745a84e6b33dd65f2ba5
endlmk <1269467+endlmk@users.noreply.github.com>
2023-04-20 23:41:09 +0900

    Add property based test for double division.

--- PropertyBasedTestingWithFSCheck/Tests.fs
+++ PropertyBasedTestingWithFSCheck/Tests.fs
@@ -14,3 +14,11 @@ let ``Add test`` () =
 [<Property>]
 let ``add = x + y`` x y = 
     add x y = x + y
+
+type DoubleModPair = 
+    static member UInt64 () =
+        Gen.choose (1, 1000000) |> Gen.two |> Gen.map (fun (x, y) -> ((uint64)x * (uint64)y, (uint64)y)) |> Arb.fromGen
+
+[<Property( Arbitrary = [| typeof<DoubleModPair> |], MaxTest=1000000)>]
+let ``double divide calcuration keeps integer precision`` (x:uint64) (y:uint64) = 
+    (uint64)((double)x / (double)y) = x / y
